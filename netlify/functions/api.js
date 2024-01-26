a14f727e94eed49a6bef9cb3dc88ad4ee5e71ecc
Martin <martinsbox@gmail.com>
2024-01-26 19:39:57 +0200

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -6,10 +6,21 @@ const api = express();
 const router = Router();
 // router.get("/hello", (req, res) => res.send("Hello World!"));
 
+// create array of tasks to be used as dummy task data
+const tasks = [
+    {id: nanoid(), title: "Create back end API for task App", completed: false},
+    {id: nanoid(), title: "Create UI for task App", completed: false}
+];
+
 // create / route which runs as soon as app starts
 router.get("/", (req, res) => {
     res.send("Welcome to my Task API server built with NodeJS and ExpressJS");
-})
+});
+
+// get all tasks saved locally in tasks array
+router.get("/tasks", (req, res) => {
+    res.send(tasks)
+});
 
 api.use("/api/", router);
 
